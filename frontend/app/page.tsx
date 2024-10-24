471e4df255cd4f83a1388f3f9743d9839bff0584
mryunt02 <mrthresh58@gmail.com>
2024-10-24 14:31:01 +0300

    auth bugs fixed

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,39 @@
-// app/page.tsx (or wherever your Home component is located)
 'use client';
 
-import React from 'react';
-import Header from '@/components/header';
-import TimeComponent from '@/components/time-component';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  return <div>qweqwewq</div>;
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      router.push('/login'); // Redirect to the login page if no token is found
+    } else {
+      setIsAuthenticated(true); // Set authentication state to true if token is found
+    }
+  }, [router]);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token'); // Remove the token from localStorage
+    router.push('/login'); // Redirect to the login page
+  };
+
+  if (!isAuthenticated) {
+    return null; // Render nothing while checking authentication
+  }
+
+  return (
+    <div>
+      <h1>Here is my home page</h1>
+      <button
+        onClick={handleLogout}
+        className='bg-slate-500 text-white px-4 py-2 rounded hover:bg-slate-400'
+      >
+        Log Out
+      </button>
+    </div>
+  );
 }
