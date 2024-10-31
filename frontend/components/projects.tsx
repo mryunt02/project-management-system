628948a117552bbd409fc156fbfc34cb5ce0f152
mryunt02 <mrthresh58@gmail.com>
2024-10-31 20:00:25 +0300

    projects dynamic links added

--- frontend/components/projects.tsx
+++ frontend/components/projects.tsx
@@ -3,12 +3,19 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProjects } from '@/redux/reducers/projectReducer'; // Ensure the path is correct
 import Project from './project';
-
+import Link from 'next/link';
+interface Project {
+  description: string;
+  members: string[];
+  name: string;
+  type: string;
+  _id: string;
+}
 const Projects = () => {
   const dispatch = useDispatch();
   const { projects, loading, error } = useSelector(
     (state: {
-      projects: { projects: any[]; loading: boolean; error: string | null };
+      projects: { projects: Project; loading: boolean; error: string | null };
     }) => state.projects
   );
   console.log(projects);
@@ -27,9 +34,11 @@ const Projects = () => {
 
   return (
     <ul className='grid grid-cols-3 gap-5'>
-      {projects.map((project: { id: number; name: string }) => (
-        <li key={project.id}>
-          <Project name={project.name} />
+      {projects?.map((project: { _id: number; name: string }) => (
+        <li key={project._id}>
+          <Link href={`/projects/${project._id}`}>
+            <Project name={project.name} />
+          </Link>
         </li>
       ))}
     </ul>
