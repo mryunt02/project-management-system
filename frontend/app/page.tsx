04cc3c242de884f719f3355354a351480b23529c
mryunt02 <mrthresh58@gmail.com>
2024-10-31 19:16:17 +0300

    ui updated

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,6 +5,9 @@ import { useRouter } from 'next/navigation';
 import { useDispatch, useSelector } from 'react-redux';
 import type { RootState } from '@/redux/store'; // Adjust the path as necessary
 import { login } from '@/redux/reducers/authReducer';
+import { Input } from '@/components/ui/input';
+import Image from 'next/image';
+import imgbg from '@/images/bgimg.png';
 
 export default function Home() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -38,11 +41,37 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <h1>Welcome, </h1>
-      <p>
-        Here is {user?.name} {user?.surname}&apos;s project page
-      </p>
+    <div className='bg-blue-100 w-[80%] m-auto mt-5'>
+      <div>
+        <p className='text-center'>You dont have any projects yet.</p>
+      </div>
+      <div>
+        <div className='flex items-center justify-between'>
+          <h1>Projects</h1>
+          <Input type='text' placeholder='Search projects' className='w-auto' />
+        </div>
+        <div className='mt-5'>
+          <ul className='flex gap-5'>
+            <li className='bg-slate-500 w-[25%]'>
+              <div className='bg-slate-500 w-full flex items-center justify-center h-[80px]'>
+                <p>Create New Project</p>
+              </div>
+            </li>
+            <li className='w-[25%]'>
+              <div className='relative'>
+                <Image
+                  src={imgbg}
+                  alt='project image'
+                  className='h-[80px] opacity-90 object-cover'
+                />
+                <p className='absolute top-1 left-3 text-white w-[94%]'>
+                  Project 1
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
