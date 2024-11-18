673ce8e41b69a3c7e813065c746839c8872005ab
mryunt02 <mrthresh58@gmail.com>
2024-11-19 00:31:44 +0300

    ui updated

--- frontend/components/TaskCard.tsx
+++ frontend/components/TaskCard.tsx
@@ -1,3 +1,5 @@
+import { Plus } from 'lucide-react';
+
 interface TaskCardProps {
   title: string;
   description: string;
@@ -5,7 +7,7 @@ interface TaskCardProps {
 
 const TaskCard = ({ title, description }: TaskCardProps) => {
   return (
-    <div className='bg-[#22272b] p-3 rounded-lg shadow-sm hover:bg-[#22272b]/80 cursor-pointer w-full text-start'>
+    <div className='bg-[#22272b] p-3 rounded-lg shadow-sm hover:bg-[#22272b]/80 cursor-pointer w-full text-start whitespace-normal'>
       <h3 className='text-[#b6c2cf] text-sm font-medium'>{title}</h3>
       <p className='text-gray-400 text-xs mt-1'>{description}</p>
     </div>
