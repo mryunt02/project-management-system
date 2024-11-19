1b407948cd006456696d83b3cde016e14481967e
mryunt02 <mrthresh58@gmail.com>
2024-11-19 16:08:01 +0300

    updated

--- frontend/components/project-header.tsx
+++ frontend/components/project-header.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import yuntlogogray from '@/images/yunt-logo-gray.png';
+import yuntlogogray from '@/images/yunt-logo.png';
 import { usePathname, useRouter } from 'next/navigation';
 
 const Header = () => {
